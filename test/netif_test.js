ca2a8c8bec0a075ffaf0c0d65c39daae9cb5d277
Mark Wolfe <mark@wolfe.id.au>
2012-12-19 20:18:28 +1100

    Defaulted tests to use linux interface names.

--- test/netif_test.js
+++ test/netif_test.js
@@ -8,9 +8,9 @@ exports['netif'] = {
   'should look up interface given an ethernet interface name': function(test) {
     test.expect(1);
     // tests here
-    //test.ok(netif.getMacAddress('eth0'), "should not error out"); // linux
+    test.ok(netif.getMacAddress('eth0'), "should not error out"); // linux
     //test.ok(netif.getMacAddress('net0'), "should not error out"); // solaris
-    test.ok(netif.getMacAddress('en0'), "should not error out"); // osx
+    //test.ok(netif.getMacAddress('en0'), "should not error out"); // osx
     test.done();
   }
 };
