58ac1b4c3784134616fbbd629db07c3022777486
Mark Wolfe <mark@wolfe.id.au>
2013-03-26 12:01:09 +1100

    Updated tests to work on the supported platforms.

--- test/netif_test.js
+++ test/netif_test.js
@@ -1,5 +1,7 @@
 var netif = require('../lib/netif');
 
+var os = require('os');
+
 exports['netif'] = {
   setUp: function(done) {
     // setup here
@@ -8,9 +10,21 @@ exports['netif'] = {
   'should look up interface given an ethernet interface name': function(test) {
     test.expect(1);
     // tests here
-    test.ok(netif.getMacAddress('eth0'), "should not error out"); // linux
+    switch(os.platform()) {
+      case 'darwin':
+        test.ok(netif.getMacAddress('en0'), "should not error out"); // osx
+        break;
+      case 'linux':
+        test.ok(netif.getMacAddress('eth0'), "should not error out"); // linux
+        break;
+      case 'solaris':
+        test.ok(netif.getMacAddress('net0'), "should not error out"); // solaris
+        break;
+      default:
+        throw Error('unsupported os for this module.');
+    }
+    test.done();
     //test.ok(netif.getMacAddress('net0'), "should not error out"); // solaris
     //test.ok(netif.getMacAddress('en0'), "should not error out"); // osx
-    test.done();
   }
 };
